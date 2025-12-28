0cd7e2af3194982ac719d0cabb7ca9e3ad2266db
shakil ahmed <shakilahmed.pbl@gmail.com>
2025-12-29 00:10:34 +0600

    commit

--- blogs/models.py
+++ blogs/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    category_name = models.CharField(max_length=100, unique=True)
     craeted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['category_name']
+
+    def __str__(self):
+        return self.category_name
+
+STATUS_CHOICES=[
+    (0,"Draft"),
+    (1,"Published")
+]    
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    slug=models.SlugField(max_length=150,unique=True,blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    featured_image=models.ImageField(upload_to='uploads/%y/%m/%d')
+    short_descriptions=models.TextField(max_length=500)
+    blog_body=models.TextField(max_length=2000)
+    status=models.IntegerField(choices=STATUS_CHOICES,default=0)
+    is_featured=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
