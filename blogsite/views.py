359be08b85ddeb6b60cb5ca9804a245d28e18831
shakil ahmed <shakilahmed.pbl@gmail.com>
2025-12-31 01:12:38 +0600

    66

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from blogs.models import Category,Blog
 
 def home(request):
-    return render(request,'home/home.html')
+    categories=Category.objects.all()
+    featured_post=Blog.objects.filter(is_featured=True,status="Published")
+    sample_post=Blog.objects.filter(status="Published",is_featured=False)
+    context={
+        "categories":categories,
+        "featured_post":featured_post,
+        "sample_post":sample_post
+    }
+  
+    return render(request,'home/home.html',context)
